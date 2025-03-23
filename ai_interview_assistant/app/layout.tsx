683d991817bdac7dd34d1066db63353545fc25a1
Nemanja Stojanovic <feemail042@gmail.com>
2025-03-23 16:11:39 +0100

    initial commit

---
+++ ai_interview_assistant/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Mona_Sans } from "next/font/google";
+import "./globals.css";
+
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Ask Guru",
+  description: "AI Interview Assistant",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body className={`${monaSans.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
